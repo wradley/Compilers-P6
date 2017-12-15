b443a8042d63f88ffb1df8ce08eb4256e0b5d3b0
Whelan Callahan <callahan@royal-02.cs.wisc.edu>
2017-12-14 20:34:52 -0600

    Added test program

--- files/mips.s
+++ files/mips.s
@@ -1,31 +1,15 @@
 	.text
-	.globl main
-main:
-__start:
-		# Entering Main
+_clear:
+		# Entering Funtion clear
 	sw    $ra, 0($sp)	#PUSH
 	subu  $sp, $sp, 4
 	sw    $fp, 0($sp)	#PUSH
 	subu  $sp, $sp, 4
 	addu  $fp, $sp, 8
 	subu  $sp, $sp, 8
-		# Main Body
+		# Function Body
 		# Assignment Start
 	la    $t0, -8($fp)
-	sw    $t0, 0($sp)	#PUSH
-	subu  $sp, $sp, 4
-	li    $t0, 10
-	sw    $t0, 0($sp)	#PUSH
-	subu  $sp, $sp, 4
-	lw    $t1, 4($sp)	#POP
-	addu  $sp, $sp, 4
-	lw    $t0, 4($sp)	#POP
-	addu  $sp, $sp, 4
-	sw    $t1, 0($t0)	#Storing RHS value in LHS address
-		# Assignment End
-.L0:
-		# Greater than
-	lw    $t0, -8($fp)
 	sw    $t0, 0($sp)	#PUSH
 	subu  $sp, $sp, 4
 	li    $t0, 0
@@ -35,7 +19,21 @@ __start:
 	addu  $sp, $sp, 4
 	lw    $t0, 4($sp)	#POP
 	addu  $sp, $sp, 4
-	bgt   $t0, $t1, .L2
+	sw    $t1, 0($t0)	#Storing RHS value in LHS address
+		# Assignment End
+.L0:
+		# Less than
+	lw    $t0, -8($fp)
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	li    $t0, 100
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $t1, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	lw    $t0, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	blt   $t0, $t1, .L2
 	li    $t1, 0
 	b     .L3
 .L2:		# True section
@@ -46,27 +44,6 @@ __start:
 	lw    $t0, 4($sp)	#POP
 	addu  $sp, $sp, 4
 	beq   $t0, 0, .L1
-	la    $t0, -8($fp)
-	sw    $t0, 0($sp)	#PUSH
-	subu  $sp, $sp, 4
-	lw    $t0, -8($fp)
-	sw    $t0, 0($sp)	#PUSH
-	subu  $sp, $sp, 4
-	lw    $t0, 4($sp)	#POP
-	addu  $sp, $sp, 4
-	li    $t1, -1
-	add   $t0, $t1, $t0
-	lw    $t1, 4($sp)	#POP
-	addu  $sp, $sp, 4
-	sw    $t0, 0($t1)
-	lw    $t0, -8($fp)
-	sw    $t0, 0($sp)	#PUSH
-	subu  $sp, $sp, 4
-	lw    $a0, 4($sp)	#POP
-	addu  $sp, $sp, 4
-#Syscall for writing ints
-	li    $v0, 1
-	syscall
 	.data
 .L4:	.asciiz"\n"	# Storing a string literal
 	.text
@@ -78,8 +55,229 @@ __start:
 #Syscall for writing strings
 	li    $v0, 4
 	syscall
+	la    $t0, -8($fp)
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $t0, -8($fp)
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $t0, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	li    $t1, 1
+	add   $t0, $t1, $t0
+	lw    $t1, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	sw    $t0, 0($t1)
 	b     .L0
 .L1:
+		# Exiting Function clear
+clear_end:
+	lw    $ra, 0($fp)
+	move  $t0, $fp
+	lw    $fp, -4($fp)
+	move  $sp, $t0
+	jr    $ra
+	.text
+_drawArt:
+		# Entering Funtion drawArt
+	sw    $ra, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	sw    $fp, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	addu  $fp, $sp, 8
+	subu  $sp, $sp, 4
+		# Function Body
+	jal   _clear		#Jumping to function
+	sw    $v0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	.data
+.L5:	.asciiz"    :)\n\n\n\n"	# Storing a string literal
+	.text
+	la    $t0, .L5		#Pushing address of string literal on the stack
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $a0, 4($sp)	#POP
+	addu  $sp, $sp, 4
+#Syscall for writing strings
+	li    $v0, 4
+	syscall
+		# Exiting Function drawArt
+drawArt_end:
+	lw    $ra, 0($fp)
+	move  $t0, $fp
+	lw    $fp, -4($fp)
+	move  $sp, $t0
+	jr    $ra
+	.text
+	.globl main
+main:
+__start:
+		# Entering Main
+	sw    $ra, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	sw    $fp, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	addu  $fp, $sp, 8
+	subu  $sp, $sp, 12
+		# Main Body
+		# Assignment Start
+	la    $t0, -12($fp)
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	li    $t0, 1
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $t1, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	lw    $t0, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	sw    $t1, 0($t0)	#Storing RHS value in LHS address
+		# Assignment End
+	jal   _clear		#Jumping to function
+	sw    $v0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+.L6:
+	lw    $t0, -12($fp)
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $t0, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	beq   $t0, 0, .L7
+	.data
+.L8:	.asciiz"[1] Clear Screen\n"	# Storing a string literal
+	.text
+	la    $t0, .L8		#Pushing address of string literal on the stack
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $a0, 4($sp)	#POP
+	addu  $sp, $sp, 4
+#Syscall for writing strings
+	li    $v0, 4
+	syscall
+	.data
+.L9:	.asciiz"[2] End Program\n"	# Storing a string literal
+	.text
+	la    $t0, .L9		#Pushing address of string literal on the stack
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $a0, 4($sp)	#POP
+	addu  $sp, $sp, 4
+#Syscall for writing strings
+	li    $v0, 4
+	syscall
+	.data
+.L10:	.asciiz"[3] For Art\n"	# Storing a string literal
+	.text
+	la    $t0, .L10		#Pushing address of string literal on the stack
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $a0, 4($sp)	#POP
+	addu  $sp, $sp, 4
+#Syscall for writing strings
+	li    $v0, 4
+	syscall
+	li    $v0, 5
+	syscall
+	la    $t0, -8($fp)
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $t0, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	sw    $v0, 0($t0)
+	lw    $t0, -8($fp)
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	li    $t0, 0
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $t1, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	lw    $t0, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	bne   $t0, $t1, .L12
+	li    $t0, 0
+	b     .L13
+.L12:		# true section
+	li    $t0, 1
+.L13:		# finish section
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $t0, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	beq   $t0, 0, .L11
+	lw    $t0, -8($fp)
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	li    $t0, 1
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $t1, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	lw    $t0, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	beq   $t0, $t1, .L16
+	li    $t0, 0
+	b     .L17
+.L16:		# true section
+	li    $t0, 1
+.L17:		# finish section
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $t0, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	beq   $t0, 0, .L14
+	jal   _clear		#Jumping to function
+	sw    $v0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	b     .L15
+.L14:
+	lw    $t0, -8($fp)
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	li    $t0, 2
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $t1, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	lw    $t0, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	beq   $t0, $t1, .L20
+	li    $t0, 0
+	b     .L21
+.L20:		# true section
+	li    $t0, 1
+.L21:		# finish section
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $t0, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	beq   $t0, 0, .L18
+	jal   _clear		#Jumping to function
+	sw    $v0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+		# Assignment Start
+	la    $t0, -12($fp)
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	li    $t0, 0
+	sw    $t0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+	lw    $t1, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	lw    $t0, 4($sp)	#POP
+	addu  $sp, $sp, 4
+	sw    $t1, 0($t0)	#Storing RHS value in LHS address
+		# Assignment End
+	b     .L19
+.L18:
+	jal   _drawArt		#Jumping to function
+	sw    $v0, 0($sp)	#PUSH
+	subu  $sp, $sp, 4
+.L19:
+.L15:
+.L11:
+	b     .L6
+.L7:
 		# Exiting Main
 	lw    $ra, 0($fp)
 	move  $t0, $fp
